8f7963f3489de28ec1741e86f0c05884285298e0
stickmanjob <stickmanjob@gmail.com>
2019-05-23 23:59:03 +0900

    Ver.005 正規表現強化

--- src/components/ResultOutput.vue
+++ src/components/ResultOutput.vue
@@ -15,8 +15,10 @@ export default {
   },
   computed: {
     formatting () {
-      var inputText = this.$store.getters.getInputText
-      var words = inputText.split(' ')
+      var inputText = this.$store.getters.getInputText // 入力文字
+      var words = ''
+      var beforeWord = ''
+      var afterWord = ''
       var outputText = ''
       var sentense = ['']
       var sentenseIndex = 0
@@ -26,48 +28,48 @@ export default {
       // **************************************************************
       // ■ keywordsオブジェクト使われ方
       // ・単語がvalueの値と一致した場合「改行」、「行間」について判定される
-      //   次の単語がnoBreakWordsだった場合改行しない
-      //   前の単語がspacedWordsだった場合行間を開ける(追加で改行する)
+      //   次の単語がnoBreakWordsと一致した場合改行しない
+      //   前の単語がspacedWordsと一致した場合行間を開ける(追加で改行する)
       // **************************************************************
       var keywords = {
-        equal: {value: '=', noBreakWords: ['#ALL'], spacedWords: []},
-        openParentheses: {value: '(', noBreakWords: [], spacedWords: []},
-        closeParentheses: {value: ')', noBreakWords: ['AS'], spacedWords: []},
-        as: {value: 'AS', noBreakWords: ['#ALL'], spacedWords: []},
-        select: {value: 'SELECT', noBreakWords: ['TOP', 'DISTINCT'], spacedWords: ['UNION', 'ALL']},
-        from: {value: 'FROM', noBreakWords: [], spacedWords: []},
-        where: {value: 'WHERE', noBreakWords: [], spacedWords: ['#ALL']},
-        inner: {value: 'INNER', noBreakWords: ['JOIN'], spacedWords: ['#ALL']},
-        left: {value: 'LEFT', noBreakWords: ['OUTER', 'JOIN'], spacedWords: ['#ALL']},
-        outer: {value: 'OUTER', noBreakWords: ['JOIN'], spacedWords: []},
-        top: {value: 'TOP', noBreakWords: ['#ALL'], spacedWords: []},
-        union: {value: 'UNION', noBreakWords: ['ALL'], spacedWords: ['#ALL']},
-        full: {value: 'FULL', noBreakWords: ['OUTER', 'JOIN'], spacedWords: ['#ALL']},
-        others: {value: '#ALL', noBreakWords: ['=', 'AS'], spacedWords: []}
+        operator: {value: /^[=+<>\-%!]{1,2}$/, noBreakWords: /^.*$/i, spacedWords: /^(?!.*)$/i},
+        openParentheses: {value: /^\($/i, noBreakWords: /^(?!.*)$/i, spacedWords: /^(?!.*)$/i},
+        closeParentheses: {value: /^\)$/i, noBreakWords: /^AS$/i, spacedWords: /^(?!.*)$/i},
+        as: {value: /^AS$/i, noBreakWords: /^.*$/i, spacedWords: /^(?!.*)$/i},
+        select: {value: /^SELECT$/i, noBreakWords: /^(TOP|DISTINCT)$/i, spacedWords: /^(UNION|ALL)$/i},
+        from: {value: /^FROM$/i, noBreakWords: /^(?!.*)$/i, spacedWords: /^(?!.*)$/i},
+        where: {value: /^WHERE$/i, noBreakWords: /^(?!.*)$/i, spacedWords: /^.*$/i},
+        inner: {value: /^INNER$/i, noBreakWords: /^JOIN$/i, spacedWords: /^.*$/i},
+        left: {value: /^LEFT$/i, noBreakWords: /^(OUTER|JOIN)$/i, spacedWords: /^.*$/i},
+        outer: {value: /^OUTER$/i, noBreakWords: /^JOIN$/i, spacedWords: /^(?!.*)$/i},
+        top: {value: /^TOP$/i, noBreakWords: /^.*$/i, spacedWords: /^(?!.*)$/i},
+        union: {value: /^UNION$/i, noBreakWords: /^.*$/i, spacedWords: /^.*$/i},
+        full: {value: /^FULL$/i, noBreakWords: /^(OUTER|JOIN)$/i, spacedWords: /^.*$/i},
+        others: {value: /.*$/i, noBreakWords: /^([=,+,<,>,-,%,!]+|AS)$/i, spacedWords: /^(?!.*)$/i}
       }
+      // 入力文字列を整えて、単語ごとに配列に格納(半角スペース区切り)
+      words = inputText.replace(/\n+/, ' ').replace(/\s+/, ' ').split(' ')
+      // 文字列整形スタート
       words.forEach((word, index, array) => {
         // 基本:単語毎に改行,行間は空けない
         breakDiv = true
         spacedDiv = false
+        beforeWord = words[index - 1]
+        afterWord = words[index + 1]
         for (let keyword of Object.keys(keywords)) {
-          if (word === keywords[keyword].value || keywords[keyword].value === '#ALL') {
-            for (let spacedWord of keywords[keyword].spacedWords) {
-              // 前の単語が行間開ける語だった場合のみ追加で改行を行う
-              if (words[index - 1] === spacedWord || spacedWord === '#ALL') {
-                spacedDiv = true
-                break
-              }
+          console.log(keywords[keyword].value.test(word))
+          if (keywords[keyword].value.test(word)) {
+            // 前の単語が行間開ける語だった場合のみ追加で改行を行う
+            if (keywords[keyword].spacedWords.test(beforeWord)) {
+              spacedDiv = true
             }
-            for (let noBreakWord of keywords[keyword].noBreakWords) {
-              // 次の単語が改行禁止語だった場合のみ改行しない
-              if (words[index + 1] === noBreakWord || noBreakWord === '#ALL') {
-                console.log('word:' + word)
-                console.log('words[index + 1]:' + words[index + 1])
-                console.log('index:' + index)
-                word = word + ' '
-                breakDiv = false
-                break
-              }
+            // 次の単語が改行禁止語だった場合のみ改行しない
+            if (keywords[keyword].noBreakWords.test(afterWord)) {
+              console.log('word:' + word)
+              console.log('words[index + 1]:' + afterWord)
+              console.log('index:' + index)
+              word = word + ' '
+              breakDiv = false
             }
             if (spacedDiv === true || breakDiv === false) break
           }
@@ -85,7 +87,7 @@ export default {
         // インデントを文に反映
         if (indentCount > 0) {
           // 現在のインデントを削除
-          sentense[sentenseIndex] = sentense[sentenseIndex].replace(/^\t*/, '')
+          sentense[sentenseIndex] = sentense[sentenseIndex].replace(/^\t*/i, '')
           for (var i = 0; i < indentCount; i++) {
             sentense[sentenseIndex] = '\t' + sentense[sentenseIndex]
           }
